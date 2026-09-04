8dd55b3863848ad8d245097d08799e8e3acb3897
Yue Wang <1939455790@qq.com>
2026-09-05 07:53:40 +0800

    docs(agent): document CodeMetrics in metrics_types.py

--- agentuniverse/agent/action/knowledge/doc_processor/types/metrics_types.py
+++ agentuniverse/agent/action/knowledge/doc_processor/types/metrics_types.py
@@ -10,6 +10,15 @@ from typing import TypedDict
 
 
 class CodeMetrics(TypedDict):
+    """Typed dictionary describing basic metrics of a code snippet.
+
+    Attributes:
+        line_count: Total number of lines in the snippet.
+        code_line_count: Number of non-empty, non-comment code lines.
+        avg_line_length: Average length of the code lines.
+        max_line_length: Length of the longest code line.
+        character_count: Total number of characters in the snippet.
+    """
     line_count: int
     code_line_count: int
     avg_line_length: float
