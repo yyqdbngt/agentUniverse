efb87c140cd04a0ea7fb60507edc33574cea130c
Yue Wang <1939455790@qq.com>
2026-09-05 08:16:14 +0800

    style(base): strip trailing whitespace in knowledge_configer.py

--- agentuniverse/base/config/component_configer/configers/knowledge_configer.py
+++ agentuniverse/base/config/component_configer/configers/knowledge_configer.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 # @Time    : 2024/3/13 12:01
-# @Author  : jerry.zzw 
+# @Author  : jerry.zzw
 # @Email   : jerry.zzw@antgroup.com
 # @FileName: knowledge_configer.py
 from typing import Optional, Dict, List
