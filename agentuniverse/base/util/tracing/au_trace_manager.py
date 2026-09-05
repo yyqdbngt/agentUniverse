9f2d6ed33db5e9995aab140d5c48d29fcb1f6fec
Yue Wang <1939455790@qq.com>
2026-09-05 08:16:28 +0800

    docs: add docstring to __getattr__ in au_trace_manager

--- agentuniverse/base/util/tracing/au_trace_manager.py
+++ agentuniverse/base/util/tracing/au_trace_manager.py
@@ -11,6 +11,8 @@ from agentuniverse.base.tracing import au_trace_manager as new_module
 
 
 def __getattr__(name):
+    """Forward attribute access to the re-exported module with a deprecation warning.
+    """
     if hasattr(new_module, name):
         warnings.warn(
             f"Importing {name} from 'agentuniverse.base.util.tracing.au_trace_manager' "
